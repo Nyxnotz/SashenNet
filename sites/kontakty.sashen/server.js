55163074d48929483e907ec23aa257583ea913cb
Nyxnotz <newaleksandrstefancev6@gmail.com>
2026-05-19 13:10:06 +0300

    Update server.js

--- sites/kontakty.sashen/server.js
+++ sites/kontakty.sashen/server.js
@@ -59,7 +59,8 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com"],
-      scriptSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       imgSrc: ["'self'", "data:"],
     }
   }
